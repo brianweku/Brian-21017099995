65408a125cfea682294606ea8e8333e3855a0e8b
brianweku <brian.weku@yahoo.com>
2017-09-20 09:26:42 +0100

    added fibbonaci

--- Palindrome.py
+++ Palindrome.py
@@ -1,5 +1,5 @@
 #Palindrome
-x='madAm'
+x=input("input word: ")
 x=x.casefold()
 reversex=reversed(x)
 if list(x) == list(reversex):
@@ -8,3 +8,12 @@ else:
     print ("False")
 
 #Fibbonaci
+x=[0, 1]
+for i in range(1, 15):
+    store=x[i-1] + x[i]
+    x.append(store)
+print(x)
+
+
+
+
